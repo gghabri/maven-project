9ac0383a432120531e6aacca27d5576ae0219505
GharbiG <ghada.gharbi@sensinov.com>
2020-06-03 18:34:51 +0200

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,14 @@ public class Greeter {
   public Greeter() {
 
   }
+  
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+    @param someone name
+    @return a string including someonename
+  */
+  
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
